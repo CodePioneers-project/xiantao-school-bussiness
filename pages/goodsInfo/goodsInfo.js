db8d9c390736a565f49f48ceca2412e526c69f2c
ArisaTaki <570490640@qq.com>
2024-11-12 22:40:06 +0800

    finish： contact

--- pages/goodsInfo/goodsInfo.js
+++ pages/goodsInfo/goodsInfo.js
@@ -1,17 +1,84 @@
 Page({
   data: {
-    messages: [], // 消息列表
-    messageInput: '', // 输入框内容
+    messages: [
+      {
+        type: 'general',
+        sender: 'other', // other表示对方，self表示自己
+        avatar: '../../img/other_avatar.png',
+        text: '这是对方发来的普通消息',
+        time: '2024-10-16 14:20'
+      },
+      {
+        type: 'general',
+        sender: 'self',
+        avatar: '../../img/self_avatar.png',
+        text: '这是我发出的普通消息',
+        time: '2024-10-16 14:22'
+      },
+      {
+        type: 'payment',
+        text: '我已付款，请确认取货信息',
+        details: {
+          location: '教学楼B栋',
+          time: '2024年10月24日 16:20',
+        },
+        time: '2024-10-16 15:20'
+      },
+      {
+        type: 'confirmation',
+        title: '我已确认',
+        text: '请按时交接',
+        sender: 'other',
+        avatar: '../../img/seller_avatar.png',
+        time: '2024-10-16 15:22'
+      },
+      {
+        type: 'completion',
+        text: '你已确认收货，交易成功',
+        time: '2024-10-24 16:22'
+      }
+    ],
+    messageInput: '',
+    emojiList: ["😀", "😂", "😊", "😍", "😎", "🤔", "😢", "😡", "👍", "👎"],
+    showEmojiPicker: false,
+    showMoreOptions: false
   },
-  
-  handleInput(e) {
+
+  openEmoji() {
+    if (this.data.showMoreOptions) {
+      this.setData({
+        showMoreOptions: !this.data.showMoreOptions
+      })
+    }
     this.setData({
-      messageInput: e.detail.value,
+      showEmojiPicker: !this.data.showEmojiPicker
     });
   },
 
+  // 选择 Emoji 表情
+  selectEmoji(e) {
+    const { emoji } = e.currentTarget.dataset;
+    this.setData({
+      messageInput: this.data.messageInput + emoji,
+      showEmojiPicker: false // 选择后隐藏 Emoji 选择器
+    });
+  },
+  // 输入框变化事件
+  handleInput(e) {
+    this.setData({
+      messageInput: e.detail.value
+    });
+  },
+
+  // 发送消息
   send() {
-    // 发送消息逻辑
+    const { messageInput, messages } = this.data;
+    if (messageInput.trim()) {
+      this.setData({
+        messages: [...messages, { type: 'general', sender: 'self', text: messageInput, time: new Date().toLocaleString() }],
+        messageInput: ''
+      });
+    }
   },
 
   handlePay() {
@@ -21,17 +88,32 @@ Page({
     });
   },
 
-  openEmoji() {
-    wx.showToast({
-      title: '打开表情选择器',
-      icon: 'none',
+  moreOptions() {
+    if (this.data.showEmojiPicker) {
+      this.setData({
+        showEmojiPicker: !this.data.showEmojiPicker
+      })
+    }
+    this.setData({
+      showMoreOptions: !this.data.showMoreOptions
     });
   },
 
-  moreOptions() {
-    wx.showToast({
-      title: '更多选项',
-      icon: 'none',
-    });
-  },
+    // 拍照事件
+    takePhoto() {
+      wx.showToast({
+        title: '打开相机',
+        icon: 'none'
+      });
+      this.setData({ showMoreOptions: false }); // 关闭弹出层
+    },
+  
+    // 选择相册事件
+    chooseAlbum() {
+      wx.showToast({
+        title: '打开相册',
+        icon: 'none'
+      });
+      this.setData({ showMoreOptions: false }); // 关闭弹出层
+    }
 });
