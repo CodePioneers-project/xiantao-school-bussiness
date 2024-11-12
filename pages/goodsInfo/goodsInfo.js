1cff7095ad3001e14f8fb07d91c0fefb8d0c48d7
ArisaTaki <570490640@qq.com>
2024-11-12 21:11:40 +0800

    init: contact-ui

--- pages/goodsInfo/goodsInfo.js
+++ pages/goodsInfo/goodsInfo.js
@@ -1,38 +1,37 @@
 Page({
   data: {
-    id: null,
-    messages: [],
-    messageInput: ''
+    messages: [], // 消息列表
+    messageInput: '', // 输入框内容
   },
-
-  onLoad: function(options) {
-    this.setData({ id: options.id });
-  },
-
-  // 发送消息
-  send: function(e) {
-    const message = this.data.messageInput.trim();
-    if (message !== '') {
-      this.setData({
-        messages: [...this.data.messages, { type: 'user', text: message }],
-        messageInput: '' // 清空输入框
-      });
-      // 发送消息到服务器（示例）
-      wx.request({
-        url: '/sendMessage',
-        method: 'POST',
-        data: { id: this.data.id, message: message },
-        success: (res) => {
-          console.log(res.data);
-        }
-      });
-    }
-  },
-
-  // 监听输入框内容变化
-  handleInput: function(e) {
+  
+  handleInput(e) {
     this.setData({
-      messageInput: e.detail.value
+      messageInput: e.detail.value,
     });
-  }
+  },
+
+  send() {
+    // 发送消息逻辑
+  },
+
+  handlePay() {
+    wx.showToast({
+      title: '跳转到付款页面',
+      icon: 'success',
+    });
+  },
+
+  openEmoji() {
+    wx.showToast({
+      title: '打开表情选择器',
+      icon: 'none',
+    });
+  },
+
+  moreOptions() {
+    wx.showToast({
+      title: '更多选项',
+      icon: 'none',
+    });
+  },
 });
