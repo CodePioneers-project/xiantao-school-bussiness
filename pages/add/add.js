c8ccd5be80a4965e410d6e1ca89d2e2556c6c439
ArisaTaki <570490640@qq.com>
2024-11-13 14:58:41 +0800

    finish: 发布页面

--- pages/add/add.js
+++ pages/add/add.js
@@ -8,32 +8,120 @@ Page({
   data: {
     img: null,
     classId: null,
-    classification: [
-      { id: 1, name: '手机' },
-      { id: 2, name: '二手书' },
-      { id: 3, name: '电脑' },
-      { id: 4, name: '更多' }
+    classification: [{
+        id: 1,
+        name: '服装'
+      },
+      {
+        id: 2,
+        name: '美妆'
+      },
+      {
+        id: 3,
+        name: '饮食'
+      },
+      {
+        id: 4,
+        name: '饰品'
+      },
+      {
+        id: 5,
+        name: '鞋包'
+      },
+      {
+        id: 6,
+        name: '生活用品'
+      },
+      {
+        id: 7,
+        name: '数码电器'
+      },
+      {
+        id: 8,
+        name: '学习用品'
+      },
+      {
+        id: 9,
+        name: '交通工具'
+      },
+      {
+        id: 10,
+        name: '体育用品'
+      },
+      {
+        id: 11,
+        name: '其他'
+      },
     ],
-    index: 0 // 当前选中的分类索引
+    statusId: null,
+    status: [{
+        id: 1,
+        name: '全新'
+      },
+      {
+        id: 2,
+        name: '翻新'
+      },
+      {
+        id: 3,
+        name: '接近新品'
+      },
+      {
+        id: 4,
+        name: '正常模型'
+      },
+      {
+        id: 5,
+        name: '略有瑕疵'
+      },
+    ],
+    locationId: null,
+    location: [{
+        id: 1,
+        name: '常州信息职业技术学院'
+      },
+      // 这里是由API获取附近学院进行拼接的。
+      {
+        id: 2,
+        name: '常州机电职业技术学院'
+      },
+      {
+        id: 3,
+        name: '常州工程职业技术学院'
+      },
+      {
+        id: 4,
+        name: '常州纺织职业技术学院'
+      },
+      {
+        id: 5,
+        name: '常州工业职业技术学院'
+      },
+      {
+        id: 6,
+        name: '常州大学'
+      },
+    ],
+    classificationIndex: 0, // 当前选中的分类索引,
+    statusIndex: 0,
+    locationIndex: 0,
+    textareaLength: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  },
+  onShow: function () {},
 
   updataimg: function () {
     var that = this;
@@ -47,6 +135,12 @@ Page({
         console.log(res.tempFiles);
       }
     });
+  },
+
+  changeTextArea(e) {
+    this.setData({
+      textareaLength: e.detail.value.length
+    })
   },
 
   formSubmit: function (e) {
@@ -74,10 +168,11 @@ Page({
         icon: "none"
       });
     } else if (that.data.classId === null) {
-      wx.showToast({
-        title: '你是不是没选分类啊',
-        icon: "none"
-      });
+      that.data.classId = 0
+    } else if (that.data.locationId === null) {
+      that.data.locationId = 0
+    } else if (that.data.statusId === null) {
+      that.data.statusId = 0
     } else {
       wx.showToast({
         title: '发布中',
@@ -109,6 +204,22 @@ Page({
     that.setData({
       index: e.detail.value,
       classId: that.data.classification[e.detail.value].id
+    });
+  },
+  bindStatusPickerChange: function (e) {
+    var that = this;
+    console.log('携带值为', e.detail.value);
+    that.setData({
+      statusIndex: e.detail.value,
+      statusId: that.data.status[e.detail.value].id
+    });
+  },
+  bindLocationPickerChange: function (e) {
+    var that = this;
+    console.log('携带值为', e.detail.value);
+    that.setData({
+      locationIndex: e.detail.value,
+      locationId: that.data.location[e.detail.value].id
     });
   }
 });
